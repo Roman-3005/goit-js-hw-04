f35d9962cf65ab016d08235080a3f3a78ce719b2
RomanBen <postromanben@gmail.com>
2025-08-26 12:19:23 +0300

    add task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,10 @@
 function isEnoughCapacity(products, containerSize) { 
-    
+    let total = 0;
+    for (const key in products) {
+    total += products[key];
 }
-
-
+return total <= containerSize;
+}
 
 console.log(
   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
